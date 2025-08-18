b932f5f7bb25cae0ac27fae9b8761e86da0c79b0
Daniel Mueller <muellerd@meta.com>
2025-08-18 08:51:53 -0700

    Use __type macro in BPF map definitions
    Summary:
    Use the __type macro for specifying key/value types of BPF maps instead
    of `__uint(key_size, sizeof(...))` constructs. In so doing we keep
    around map type information, we shorten the code, and we silence the
    warnings pointed out by the linter after the update with D80296380.
    
    Reviewed By: sonntex
    
    Differential Revision: D80347560
    
    fbshipit-source-id: 38857088b641dbcdf13553c7dc9098156824de96

--- strobelight/bpf_lib/samples/pystacks/pystacks_sample.bpf.c
+++ strobelight/bpf_lib/samples/pystacks/pystacks_sample.bpf.c
@@ -8,8 +8,8 @@ struct event_heap_map event_heap SEC(".maps");
 
 struct {
   __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
-  __uint(key_size, sizeof(u32));
-  __uint(value_size, sizeof(u32));
+  __type(key, u32);
+  __type(value, u32);
 } samples SEC(".maps");
 
 SEC("perf_event")
