c6a0f072d44deda639ed7d31a9d4c9b3f7950f75
Jonathan Wiepert <jonathan.wiepert@gmail.com>
2025-10-10 16:47:12 -0700

    fbshipit-source-id: 52b6123d2fc200432ec59b335417d65fd88a5917

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,35 +4,32 @@ sudo apt-get install -y linux-tools-common libelf-dev linux-libc-dev \
     clang libbpf-dev make pkg-config libfmt-dev libre2-dev libcap-dev \
     --no-install-recommends
 
-git submodule update --init --recursive
-if [ $? -ne 0 ]
+if ! git submodule update --init --recursive
 then
   echo "submodule update failed"
   exit 1
 fi
 
-make -C strobelight/bpf_lib/python
-if [ $? -ne 0 ]
+if ! make -C strobelight/bpf_lib/python
 then
   echo "library build failed"
   exit 1
 fi
 
-make INSTALL_DIR=`pwd`/foo -C strobelight/bpf_lib/python install
-if [ $? -ne 0 ]
+if ! make INSTALL_DIR="$(pwd)/foo" -C strobelight/bpf_lib/python install
 then
   echo "library install failed"
   exit 1
 fi
 
-if [ `ls -l foo/pystacks.bpf.o foo/libpystacks.a | wc -l` -ne 2 ]
+if ! find foo/pystacks.bpf.o || ! find foo/libpystacks.a
 then
   echo "install did not place files where expected"
   exit 1
 fi
 
-make -C strobelight/bpf_lib/samples/pystacks/
-if [ $? -ne 0 ]
+
+if ! make -C strobelight/bpf_lib/samples/pystacks/
 then
   echo "sample build failed"
   exit 1
